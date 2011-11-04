53079574196192ce31d121791af9731a0a1f8192
rsetti <rsetti@e527132b-a56f-46a9-8d18-387a5c0b1943>
2011-11-04 11:12:31 +0000

    close parenthesis
    git-svn-id: https://mothership.svn.sourceforge.net/svnroot/mothership@170 e527132b-a56f-46a9-8d18-387a5c0b1943

--- branches/ship-daemon/mothership/configure/__init__.py
+++ branches/ship-daemon/mothership/configure/__init__.py
@@ -60,7 +60,7 @@ class Configure:
             # PostgreSQL
             if dbconfig['engine'] == 'postgresql':
                 if dbconfig['socket'] == '1':
-                    dbtuble =  (dbconfig['dbname'], dbconfig['user']
+                    dbtuble =  (dbconfig['dbname'], dbconfig['user'])
                     engine = sqlalchemy.create_engine("postgres:///%s?user=%s")
                 else:
                     dbtuple = (dbconfig['user'], dbconfig['hostname'], dbconfig['dbname'])
