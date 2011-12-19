2b439256c94ad0e9136eedced662b6cc3bbe5424
david kovach <dkovach@gilt.com>
2011-12-19 22:18:22 +0000

    stuff

--- mothership/serverinfo/__init__.py
+++ mothership/serverinfo/__init__.py
@@ -21,6 +21,7 @@
 import sys
 import mothership
 import mothership.kv
+import mothership.validate
 from mothership.mothership_models import *
 from sqlalchemy import or_, desc, MetaData
 
@@ -41,25 +42,24 @@ class ServerInfo:
                     'mothership_models': '1' # another module, and its minimum accepted version
                 },
             },
-            'functions': { # a dict of functions we're presenting to the outside world
-                'getserverinfo': { # a function identifier
+            'methods': { # a dict of methods we're presenting to the outside world
+                'search': { # a method identifier
                     'description': 'retrieve server info for a server identified by hostname.realm.site_id', # for generating help
                     'url': '/'+self.namespace, # REST url we'd like to use
                     'class': self.name, # our class name
                     'namespace': self.namespace, # our desired namespace
                     'version': self.version, # our version
-                    'call': 'getserverinfo', # the function call, stripped of () and args
+                    'call': 'get_host', # the function call, stripped of () and args
                     'required_args': { # dict of info about arguments we just can't live without
-                        'host': 'string', # an arg, and its type
-                        'realm': 'string', # another arg, and its type
-                        'site_id': 'string' # yet another arg, and its type
                     },
                     'optional_args': { # dict holding optional argument info
-                        'min': '0', # minimum number of optional args we'll accept
-                        'max': '2', # maximum number of optional args we'll accept
+                        'min': '1', # minimum number of optional args we'll accept
+                        'max': '1', # maximum number of optional args we'll accept
                         'args': { # the argument definitions themselves
-                            'test': 'int', # an arg and its type
-                            'test2': 'ip', # another arg and its type
+                            'hw_tag': 'string', # an arg and its type
+                            'ip': 'string', # an arg and its type
+                            'mac': 'string', # an arg and its type
+                            'hostname': 'string', # an arg and its type
                         },
                     },
                     'cmdln_aliases': [ # list of desired subcommand aliases for our command line
@@ -80,44 +80,11 @@ class ServerInfo:
                         ],
                     },
                 },
-                'search': {
-                    'description': 'retrieve server info, identified by one of: hw_tag (hardware tag), ip (ip address), mac (mac address)',
-                    'url': '/'+self.namespace+'/search',
-                    'class': self.name,
-                    'namespace': self.namespace,
-                    'version': self.version,
-                    'call': 'get_host',
-                    'required_args': {},
-                    'optional_args': {
-                        'min': '1',
-                        'max': '1',
-                        'args': {
-                            'hw_tag': 'string',
-                            'ip': 'string',
-                            'mac': 'string',
-                        },
-                    },
-                    'cmdln_aliases': [
-                        'si_search',
-                    ],
-                    'return': {
-                        'server': 'ORMobject',
-                        'hardware': 'ORMobject',
-                        'network': [
-                            'ORMobject',
-                            'ORMobject',
-                            ],
-                        'kv': [
-                            'ORMobject',
-                            'ORMobject',
-                        ],
-                    },
-                },
             },
         }
 
 
-    def get_host(self, hw_tag=None, ip=None, mac=None):
+    def get_host(self, hw_tag=None, ip=None, mac=None, hostname=None):
         """
         [description]
         search for a host based on info supplied
@@ -127,24 +94,26 @@ class ServerInfo:
             hw_tag: the hardware tag to search for
             ip: the ip to search for
             mac: the mac address to search for
+            hostname: the hostname.realm.site_id to search for
 
         [return value]
-        returns a hostname if successful, raises an exception if unsuccessful
+        returns a dict of ORMobjects if successful, "None" if unsuccessful
         """
 
         cfg = self.cfg
+        ret = {}
 
-        if sum(x != None for x in (hw_tag, ip, mac)) != 1:
+        if sum(x != None for x in (hw_tag, ip, mac, hostname)) != 1:
             raise ServerInfoError(
                     "get_host() takes precisely ONE value to search on.\n"
-                    "hw_tag=%s ip=%s mac=%s" % hw_tag, ip, mac)
+                    "hw_tag=%s ip=%s mac=%s hostname=%s" % (hw_tag, ip, mac, hostname))
         elif hw_tag != None:
             try:
                 s = cfg.dbsess.query(Server).\
                     filter(Server.hw_tag==hw_tag).\
                     filter(Server.virtual==False).first()
                 if s.hostname:
-                    return s.hostname
+                    ret = self.__getserverinfo(s.hostname, s.realm, s.site_id)
             except TypeError:
                 raise ServerInfoError("no host found with hw_tag: %s" % hw_tag)
         elif ip != None:
@@ -154,7 +123,7 @@ class ServerInfo:
                     filter(Server.id==Network.server_id).\
                     filter(Network.ip==ip).first()
                 if s.hostname:
-                    return s.hostname
+                    ret = self.__getserverinfo(s.hostname, s.realm, s.site_id)
             except TypeError:
                 pass
             # try the public ip
@@ -163,11 +132,9 @@ class ServerInfo:
                     filter(Server.id==Network.server_id).\
                     filter(Network.public_ip==ip).first()
                 if s.hostname:
-                    return s.hostname
+                    ret = self.__getserverinfo(s.hostname, s.realm, s.site_id)
             except TypeError:
-                pass
-            raise ServerInfoError(
-                    "no host found with public or private ip: %s" % ip)
+                raise ServerInfoError("no host found with public or private ip: %s" % ip)
         elif mac != None:
             try:
                 h, s = cfg.dbsess.query(Network, Server).\
@@ -175,18 +142,26 @@ class ServerInfo:
                     filter(Network.mac==mac).\
                     filter(Server.virtual==False).first()
                 if s.hostname:
-                    return s.hostname
+                    ret = self.__getserverinfo(s.hostname, s.realm, s.site_id)
             except TypeError:
-                pass
-            raise ServerInfoError("no host found with MAC address: %s" % mac)
+                raise ServerInfoError("no host found with MAC address: %s" % mac)
+        elif hostname != None:
+            try:
+                unqdn = mothership.validate.v_get_fqn(cfg, name=hostname)
+                s = mothership.validate.v_get_host_obj(cfg, unqdn)
+                ret = self.__getserverinfo(s.hostname, s.realm, s.site_id)
+            except:
+                raise ServerInfoError("no host found with name: %s" % hostname)
         else:
             raise ServerInfoError("You did something weird, please don't."
-                    "hw_tag=%s ip=%s mac=%s" % hw_tag, ip, mac)
+                    "hw_tag=%s ip=%s mac=%s hostname=%s" % (hw_tag, ip, mac, hostname))
 
-    def getserverinfo(self, host, realm, site_id):
+        return ret
+
+    def __getserverinfo(self, host, realm, site_id):
         """
         [description]
-        return all information for a server, identified by host.realm.site_id 
+        return all information for a server, identified by host.realm.site_id
 
         [parameter info]
         required:
@@ -195,13 +170,13 @@ class ServerInfo:
             site_id: the site_id of the server we're displaying
 
         [return value]
-        ret = a list of objects related to the server entry 
+        ret = a list of objects related to the server entry
         """
 
         cfg = self.cfg
         kvs = [] # stores kv objects to return
         nets = [] # stores network objects to return
-        ret = {} # order: [0]=server, [1]=hardware, [2]=kv, [3]=network, [4]=network, (+more network objects if there are more)
+        ret = {} # dict of objects to return (except network and kv, which are lists of objects)
 
         # gather server info from mothership's db
         try:
@@ -239,72 +214,3 @@ class ServerInfo:
           raise ServerInfoError("something horrible happened")
 
         return ret
-
-
-    def ip_only(self, host, realm, site_id):
-        """
-        [description]
-        print ip information for a server
-
-        [parameter info]
-        required:
-            host: the hostname of the server we're displaying
-            realm: the realm of the server we're displaying
-            site_id: the site_id of the server we're displaying
-
-        [return value]
-        no explicit return
-        """
-
-        cfg = self.cfg
-
-        # gather host info from mothership
-        h, s = cfg.dbsess.query(Hardware, Server).filter(Server.hostname==host).\
-        filter(Server.realm==realm).\
-        filter(Server.site_id==site_id).\
-        filter(Hardware.hw_tag==Server.hw_tag).first()
-    
-        print "host|\t%s.%s.%s" % (host, realm, site_id)
-    
-        # display the interface and associated ips
-        for n in cfg.dbsess.query(Network).\
-        filter(Server.id==Network.server_id).\
-        filter(Server.hostname==s.hostname).\
-        order_by(Network.interface).all():
-          if n.ip == None:
-            print "%s|\tprivate ip: %s\t | public ip: %s" % (n.interface, n.ip, n.public_ip)
-          else:
-            print "%s|\tprivate ip: %s\t | public ip: %s" % (n.interface, n.ip, n.public_ip)
-    
-    def mac_only(self, host, realm, site_id):
-        """
-        [description]
-        print mac address information for a server 
-    
-        [parameter info]
-        required:
-            host: the hostname of the server we're displaying
-            realm: the realm of the server we're displaying
-            site_id: the site_id of the server we're displaying
-    
-        [return value]
-        no explicit return 
-        """
-    
-        cfg = self.cfg
-
-        # gather host info from mothership
-        h, s = cfg.dbsess.query(Hardware, Server).filter(Server.hostname==host).\
-        filter(Server.realm==realm).\
-        filter(Server.site_id==site_id).\
-        filter(Hardware.hw_tag==Server.hw_tag).first()
-    
-        print "host|\t%s.%s.%s" % (host, realm, site_id)
-    
-        # display the interface and associated mac address
-        for n in cfg.dbsess.query(Network).\
-        filter(Server.id==Network.server_id).\
-        filter(Server.hostname==s.hostname).\
-        order_by(Network.interface).all():
-          print "%s|\tip: %s" % (n.interface, n.mac)
-    
