0e7f03abe15cf4e19f942f850eb486bcfbd8079d
david kovach <dkovach@gilt.com>
2011-12-29 20:39:44 +0000

    dynamic module loading

--- mothership/puppet/__init__.py
+++ mothership/puppet/__init__.py
@@ -22,106 +22,177 @@ import mothership.kv
 import mothership.users
 from mothership.mothership_models import *
 
-def classify(cfg, name):
-    """
-    Puppet external node classifier.  Returns the hash to dump as yaml.
-    """
-    classes = []
-    mtags = []
-    environment = ""
-    parameters = {}
-    groups = []
-    security_level = 0
-    server = None
-    server_id = None
-    mtag = None
-    networks = []
-    sudoers = []
-    hostname, realm, site_id = mothership.get_unqdn(cfg, name)
+class PuppetError(Exception):
+    pass
 
-    # Unique or unqualified domain name
-    unqdn = "%s.%s.%s" % (hostname, realm, site_id)
-    parameters['unqdn'] = unqdn
-    parameters['site_id'] = site_id
-    parameters['realm'] = realm
+class puppet:
 
-    # LDAP groups
-    for g in mothership.add_ldap_groups(hostname, realm, site_id):
-        groups.append(g)
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.version = 1
+        self.name = 'puppet'
+        self.namespace = 'puppet'
+        self.metadata = {
+            'config': {
+                'module_dependencies': {
+                    'mothership_models': 1,
+                    'mothership.kv': 1,
+                    'mothership.users': 1,
+                },
+            },
+            'methods': {
+                'classify': {
+                    'description': 'classify a puppet node',
+                    'call': 'classify',
+                    'required_args': {
+                        'args': {
+                            'hostname': {
+                                'vartype': 'string',
+                                'desc': 'hostname of the server to classify',
+                            },
+                        },
+                    },
+                    'optional_args': {
+                    },
+                    'cmdln_aliases': [
+                        'p',
+                        'puppet_classify',
+                        'classify_puppet',
+                    ],
+                    'return': {
+                        'node': 'dict',
+                    },
+                },
+            },
+        }
 
-    # Server
-    server = cfg.dbsess.query(Server).filter(Server.hostname==hostname).first()
-    if server:
-        mtag = cfg.dbsess.query(Tag).filter(Tag.name==server.tag).first()
 
-    if server and server.id:
-        server_id = server.id
-        parameters['server_id'] = server_id
-        networks = cfg.dbsess.query(Network).\
-                filter(Network.server_id==server_id).all()
-        for network in networks:
-            if network.interface=='eth1':
-                if network.static_route:
-                    default_gateway = network.static_route
-                    parameters['default_gateway'] = default_gateway
-                if network.ip:
-                    bond_ip = network.ip
-                    parameters['bond_ip'] = bond_ip
-                if network.netmask:
-                    bond_netmask = network.netmask
-                    parameters['bond_netmask'] = bond_netmask
-                if network.bond_options:
-                    bond_options = network.bond_options
-                    parameters['bond_options'] = bond_options
+    def classify(self, query):
+        """
+        [description]
+        search for a host based on info supplied, spit back a dict to be used to construct a puppet manifest
 
-    # Tag
-    if mtag and mtag.name:
-        mtags.append(mtag.name)
-        classes.append("tags::%s" % mtag.name)
-        parameters['mtag'] = mtag.name
+        [parameter info]
+        required:
+            query: the query dict being passed to us from the called URI
 
-    if server and server.tag_index:
-        parameters['mtag_index'] = server.tag_index
+        [return value]
+        returns a dict of information if successful
+        """
+        cfg = self.cfg
+        classes = []
+        mtags = []
+        environment = ""
+        parameters = {}
+        groups = []
+        security_level = 0
+        server = None
+        server_id = None
+        mtag = None
+        networks = []
+        sudoers = []
+        node = {}
 
-    # Security
-    if mtag and mtag.security_level != None:
-        security_level = mtag.security_level
+        # make sure we got a hostname
+        try:
+            name = query['hostname']
+            if name:
+                if cfg.debug:
+                    print "puppet/classify: querying for hostname: %s" % name
+            else:
+                if cfg.debug:
+                    print "puppet/classify: you must specify a (string) value in order to query by hostname"
+                raise PuppetError("puppet/classify: you must specify a (string) value in order to query by hostname")
 
-    if server and server.security_level != None:
-        security_level = server.security_level
+            hostname, realm, site_id = mothership.get_unqdn(cfg, name)
 
-    if security_level != None:
-        classes.append("security%s" % security_level)
-        parameters['security_level'] = security_level
+            # Unique or unqualified domain name
+            unqdn = "%s.%s.%s" % (hostname, realm, site_id)
+            parameters['unqdn'] = unqdn
+            parameters['site_id'] = site_id
+            parameters['realm'] = realm
 
-    # Key/values
-    kvs = mothership.kv.collect(cfg, unqdn)
-    for kv in kvs:
-        key = kv.key
-        value = kv.value
-        if key == 'environment':
-            environment = value
-        elif key == 'tag':
-            mtags.append(value)
-            classes.append("tags::%s" % value)
-        elif key == 'class':
-            classes.append(value)
-        elif key == 'group':
-            groups.append(value)
-        else:
-            parameters[key] = value
+            # LDAP groups
+            for g in mothership.add_ldap_groups(hostname, realm, site_id):
+                groups.append(g)
 
-    # sudoers
-    sudoers = mothership.users.gen_sudoers_groups(cfg, unqdn)
+            # Server
+            server = cfg.dbsess.query(Server).filter(Server.hostname==hostname).first()
+            if server:
+                mtag = cfg.dbsess.query(Tag).filter(Tag.name==server.tag).first()
 
-    parameters['mtags'] = mtags
-    parameters['groups'] = groups
-    if sudoers:
-        parameters['sudoers_groups'] = sudoers
+            if server and server.id:
+                server_id = server.id
+                parameters['server_id'] = server_id
+                networks = cfg.dbsess.query(Network).\
+                        filter(Network.server_id==server_id).all()
+                for network in networks:
+                    if network.interface=='eth1':
+                        if network.static_route:
+                            default_gateway = network.static_route
+                            parameters['default_gateway'] = default_gateway
+                        if network.ip:
+                            bond_ip = network.ip
+                            parameters['bond_ip'] = bond_ip
+                        if network.netmask:
+                            bond_netmask = network.netmask
+                            parameters['bond_netmask'] = bond_netmask
+                        if network.bond_options:
+                            bond_options = network.bond_options
+                            parameters['bond_options'] = bond_options
 
-    node = {}
-    node['classes'] = classes
-    node['environment'] = environment
-    node['parameters'] = parameters
+            # Tag
+            if mtag and mtag.name:
+                mtags.append(mtag.name)
+                classes.append("tags::%s" % mtag.name)
+                parameters['mtag'] = mtag.name
 
-    return node
+            if server and server.tag_index:
+                parameters['mtag_index'] = server.tag_index
+
+            # Security
+            if mtag and mtag.security_level != None:
+                security_level = mtag.security_level
+
+            if server and server.security_level != None:
+                security_level = server.security_level
+
+            if security_level != None:
+                classes.append("security%s" % security_level)
+                parameters['security_level'] = security_level
+
+            # Key/values
+            kvs = mothership.kv.collect(cfg, unqdn)
+            for kv in kvs:
+                key = kv.key
+                value = kv.value
+                if key == 'environment':
+                    environment = value
+                elif key == 'tag':
+                    mtags.append(value)
+                    classes.append("tags::%s" % value)
+                elif key == 'class':
+                    classes.append(value)
+                elif key == 'group':
+                    groups.append(value)
+                else:
+                    parameters[key] = value
+
+            # sudoers
+            sudoers = mothership.users.gen_sudoers_groups(cfg, unqdn)
+
+            parameters['mtags'] = mtags
+            parameters['groups'] = groups
+            if sudoers:
+                parameters['sudoers_groups'] = sudoers
+
+            node['classes'] = classes
+            node['environment'] = environment
+            node['parameters'] = parameters
+
+        except:
+            if cfg.debug:
+                print "puppet/classify: query failed for hostname: %s" % name
+            raise PuppetError("puppet/classify: query failed for hostname: %s" % name)
+
+        return node
--- ship_daemon.py
+++ ship_daemon.py
@@ -22,54 +22,69 @@ httpship = bottle.Bottle()
 cfg = configure.Configure('mothership.yaml')
 # set our debugging flag
 cfg.debug = cfg.debug
-# dict to hold our modules' metadata
-module_metadata = {}
-# base path we're being called from, to find our modules
-basepath = sys.path[0]
-try:
-    # get a list of all subdirectories under mothership
-    for i in os.walk(basepath+'/mothership/').next()[1]:
-        try:
-            # import each module in the list above, grab the metadata
-            # from it's main class
-            if cfg.debug:
-                print "\nimporting mothership.%s:" % i
-            mod = __import__("mothership."+i)
-            if cfg.debug:
-                print "import complete"
-            foo = getattr(mod, i)
-            if cfg.debug:
-                print foo
-            bar = getattr(foo, i)
-            if cfg.debug:
-                print bar
-            inst = bar(cfg)
-            if cfg.debug:
-                print inst
-            module_metadata[i] = inst
-        except:
-            if cfg.debug:
-                print "module \"%s\" does not have a valid main class" % i
-            else:
-                pass
-except ImportError, e:
-    print "problem importing "+i
-    print "error: "+e
+
+
+# scans our main path for modules, loads valid modules
+@httpship.route('/loadmodules')
+def load_modules():
+    """
+    scans our main path for modules, loads valid modules
+    """
+    # clear module metadata
+    cfg.module_metadata = {}
+    # base path we're being called from, to find our modules
+    basepath = sys.path[0]
+    try:
+        # get a list of all subdirectories under mothership
+        for i in os.walk(basepath+'/mothership/').next()[1]:
+            try:
+                # import each module in the list above, grab the metadata
+                # from it's main class
+                if cfg.debug:
+                    print "\nimporting mothership.%s:" % i
+                mod = __import__("mothership."+i)
+                if cfg.debug:
+                    print "import complete"
+                foo = getattr(mod, i)
+                if cfg.debug:
+                    print foo
+                bar = getattr(foo, i)
+                if cfg.debug:
+                    print bar
+                inst = bar(cfg)
+                if cfg.debug:
+                    print inst
+                cfg.module_metadata[i] = inst
+            except:
+                if cfg.debug:
+                    print "module \"%s\" does not have a valid main class" % i
+                else:
+                    pass
+    except ImportError, e:
+        print "problem importing "+i
+        print "error: "+e
+
+
+# run our module loader once at startup
+load_modules()
+
 
 @httpship.route('/')
 def index():
     buf = "<P>loaded modules:<BR><BR>"
-    for k in module_metadata.keys():
+    for k in cfg.module_metadata.keys():
         if cfg.debug:
             print 'route: /'
             print 'metadata key: '+k
         try:
-            buf += "<A HREF=\"/%s\">/%s</A><BR>" % (module_metadata[k].namespace, module_metadata[k].namespace)
+            buf += "<A HREF=\"/%s\">/%s</A><BR>" % (cfg.module_metadata[k].namespace, cfg.module_metadata[k].namespace)
             if cfg.debug:
                 print buf
         except:
             continue
+    buf += "<BR><BR>to reload modules call:<BR><A HREF=\"/loadmodules\">/loadmodules</A><BR>"
     return buf
+
 
 @httpship.route("/:pname")
 def namespace_path(pname):
@@ -78,13 +93,14 @@ def namespace_path(pname):
     if cfg.debug:
         print "buf: "+buf
         print "pname: "+pname
-        print "module_metadata[pname]: "
-        print module_metadata[pname].metadata
-    for meth in module_metadata[pname].metadata['methods']:
+        print "cfg.module_metadata[pname]: "
+        print cfg.module_metadata[pname].metadata
+    for meth in cfg.module_metadata[pname].metadata['methods']:
         if cfg.debug:
             print meth
         buf += "<A HREF=\"/%s/%s\">/%s/%s</A><BR>" % (pname, meth, pname, meth)
     return buf
+
 
 @httpship.route("/:pname/:callpath")
 def callable_path(pname, callpath):
@@ -95,55 +111,58 @@ def callable_path(pname, callpath):
     # hard wire it into callpath options
     if callpath == 'metadata':
         if cfg.debug:
-            print myjson.JSONEncoder(indent=4).encode(module_metadata[pname].metadata)
-            buf = myjson.JSONEncoder().encode(module_metadata[pname].metadata)
+            print myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata)
+            buf = myjson.JSONEncoder().encode(cfg.module_metadata[pname].metadata)
             return buf
         else:
-            buf = myjson.JSONEncoder().encode(module_metadata[pname].metadata)
+            buf = myjson.JSONEncoder().encode(cfg.module_metadata[pname].metadata)
             return buf
     # everyone wants query strings, blow up and spit out information if
     # we don't get any query strings
     elif not query:
         buf = "Here are the possible query strings for \"/%s/%s\"<BR><BR>required_args:<BR>" % (pname, callpath)
         try:
-            for req in module_metadata[pname].metadata['methods'][callpath]['required_args']['args'].keys():
+            for req in cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'].keys():
                 if cfg.debug:
                     print "required arg: %s" % req
-                buf += "%s (%s): %s<BR>" % (req, module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['vartype'], module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['desc'])
+                buf += "%s (%s): %s<BR>" % (req, cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['desc'])
         except:
             buf += "No required_args found<BR>"
-        buf += "<BR><BR>optional_args, please supply at least %s but not more than %s of the following:<BR>" % (module_metadata[pname].metadata['methods'][callpath]['optional_args']['min'], module_metadata[pname].metadata['methods'][callpath]['optional_args']['max'])
         try:
-            for opt in module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'].keys():
+            buf += "<BR><BR>optional_args, please supply at least %s but not more than %s of the following:<BR>" % (cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['min'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['max'])
+            for opt in cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'].keys():
                 if cfg.debug:
                     print "optional arg: %s" % opt
-                if module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'] == "None":
-                    buf += "<A HREF=\"/%s/%s?%s\">/%s/%s?%s</A> (%s): %s<BR>" % (pname, callpath, opt, pname, callpath, opt, module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
+                if cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'] == "None":
+                    buf += "<A HREF=\"/%s/%s?%s\">/%s/%s?%s</A> (%s): %s<BR>" % (pname, callpath, opt, pname, callpath, opt, cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
                 else:
-                    buf += "/%s/%s?%s (%s): %s<BR>" % (pname, callpath, opt, module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
+                    buf += "/%s/%s?%s (%s): %s<BR>" % (pname, callpath, opt, cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
         except:
-            buf += "No optional_args found<BR>"
+            buf += "<BR><BR>optional_args:<BR>No optional_args found<BR>"
         if cfg.debug:
             print buf
         return buf
     # we got an actual callpath! do stuff.
     else:
         if cfg.debug:
-            print "method called: %s" % myjson.JSONEncoder(indent=4).encode(module_metadata[pname].metadata['methods'][callpath]['call'])
-            buf = getattr(module_metadata[pname], module_metadata[pname].metadata['methods'][callpath]['call'])
+            print "method called: %s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath]['call'])
+            buf = getattr(cfg.module_metadata[pname], cfg.module_metadata[pname].metadata['methods'][callpath]['call'])
             print myjson.JSONEncoder(indent=4).encode(buf(query))
             return myjson.JSONEncoder().encode(buf(query))
         else:
-            buf = getattr(module_metadata[pname], module_metadata[pname].metadata['methods'][callpath]['call'])
+            buf = getattr(cfg.module_metadata[pname], cfg.module_metadata[pname].metadata['methods'][callpath]['call'])
             return myjson.JSONEncoder().encode(buf(query))
+
 
 @httpship.route("/favicon.ico")
 def favicon():
     return bottle.static_file('favicon.ico', root=sys.path[0])
 
+
 @httpship.route("/test")
 def test():
     pass
+
 
 # the daemon
 bottle.run(httpship, host='0.0.0.0', port=8081, reloader=False)
