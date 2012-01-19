afa587ad8903cbdfb18af9573347a375316ac502
Riccardo Setti <rsetti@gilt.com>
2012-01-19 12:41:53 -0500

    nojira: refactor ship_daemon (PITA)

--- ship_daemon.py
+++ ship_daemon.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import bottle
+from bottle import static_file
 from bottle import response
 
 from mothership import configure
@@ -28,14 +29,12 @@ cm = MothershipCommon('ship_daemon.log', to_file=False)
 class ShipDaemonError(Exception):
     pass
 
-# scans our main path for modules, loads valid modules
 @httpship.route('/loadmodules')
 def load_modules():
     """
     scans our main path for modules, loads valid modules
     """
-    if cfg.debug:
-        cm.debug("loadmodules() called directly")
+    cm.debug("loadmodules() called directly")
     # clear module metadata
     old_metadata = cfg.module_metadata
     cfg.module_metadata = {}
@@ -74,24 +73,20 @@ def load_modules():
         print "error: "+e
 
 
-# run our module loader once at startup
-load_modules()
-
-
-# main url, currently spits back info about loaded modules and such
-# will probably change quite a lot before the rewrite gets going
-# more than likely will merge with /modules route below
 @httpship.route('/')
 def index():
+    """
+    main url, currently spits back info about loaded modules and such
+    will probably change quite a lot before the rewrite gets going
+    more than likely will merge with /modules route below
+    """
     buf = "<P>loaded modules:<BR><BR>"
     for k in cfg.module_metadata.keys():
-        if cfg.debug:
-            print 'route: /'
-            print 'metadata key: '+k
+        cm.debug('route: /')
+        cm.debug('metadata key: '+k)
         try:
             buf += "<A HREF=\"/%s\">/%s</A><BR>" % (cfg.module_metadata[k].namespace, cfg.module_metadata[k].namespace)
-            if cfg.debug:
-                print buf
+            cm.debug(buf)
         except:
             continue
     buf += "<BR><BR>to reload modules call:<BR><A HREF=\"/loadmodules\">/loadmodules</A><BR>"
@@ -99,107 +94,99 @@ def index():
     return buf
 
 
-# returns a list of currently loaded modules
 @httpship.route('/modules')
 def loaded_modules():
+    """
+    returns a list of currently loaded modules
+    """
     buf = []
     for k in cfg.module_metadata.keys():
-        if cfg.debug:
-            print 'route: /'
-            print 'metadata key: '+k
+        cm.debug('route: /')
+        cm.debug('metadata key: '+k)
         try:
             buf.append(cfg.module_metadata[k].namespace)
-            if cfg.debug:
-                print buf
+            cm.debug(buf)
         except:
             continue
     response.content_type = 'application/json'
     return myjson.JSONEncoder().encode(buf)
 
-
-# returns data about a namespace's public functions
 @httpship.route("/:pname")
 def namespace_path(pname):
+    """
+    returns data about a namespace's public functions
+    """
     buf = "Callable functions:<BR><BR>"
     buf += "<A HREF=\"/%s/metadata\">/%s/metadata</A><BR>" % (pname, pname)
-    if cfg.debug:
-        print "buf: "+buf
-        print "pname: "+pname
-        print "cfg.module_metadata[pname]: "
-        print cfg.module_metadata[pname].metadata
+    cm.debug("buf: %s" % buf)
+    cm.debug("pname: %s" %pname)
+    cm.debug("cfg.module_metadata[pname]: %s " %cfg.module_metadata[pname].metadata)
     for meth in cfg.module_metadata[pname].metadata['methods']:
-        if cfg.debug:
-            print meth
+        cm.debug(meth)
         buf += "<A HREF=\"/%s/%s\">/%s/%s</A><BR>" % (pname, meth, pname, meth)
     return buf
 
-
-# returns data about a function call or calls the function.
-# will probably change significantly before the rewrite
 @httpship.route("/:pname/:callpath", method=('GET', 'POST', 'PUT', 'DELETE'))
 def callable_path(pname, callpath):
+    """
+    returns data about a function call or calls the function.
+    will probably change significantly before the rewrite
+    """
     query = bottle.request.GET
-    if cfg.debug:
-        print "query keys: %s" % query.keys()
+    pnameMetadata = cfg.module_metadata[pname]
+    pnameCallpath = pnameMetadata.metadata['methods'][callpath]
+    
+    cm.debug("query keys: %s" % query.keys())
     # everyone has a 'metadata' construct
     # hard wire it into callpath options
     if callpath == 'metadata':
-        if cfg.debug:
-            print myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata)
-            buf = myjson.JSONEncoder().encode(cfg.module_metadata[pname].metadata)
-            return buf
+        cm.debug(myjson.JSONEncoder(indent=4).encode(pnameMetadata.metadata))
+        buf = myjson.JSONEncoder().encode(cfg.module_metadata[pname].metadata)
+        return buf
+    
+    # we got an actual callpath! do stuff.
+    if query:
+        response.content_type='application/json'
+        cm.debug("method called: %s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath]))
+        if bottle.request.method == pnameCallpath['rest_type']:
+            buf = getattr(pnameMetadata, callpath)
+            returnme = buf(query)
+            cm.debug(myjson.JSONEncoder(indent=4).encode(returnme))
+            return myjson.JSONEncoder().encode(returnme)
         else:
-            return buf
-            buf = myjson.JSONEncoder().encode(cfg.module_metadata[pname].metadata)
-            return buf
+            raise ShipDaemonError("request method \"%s\" does not match allowed type \"%s\" for call \"/%s/%s\"" % (bottle.request.method, pnameCallpath['rest_type'], pname, callpath))
     # everyone wants query strings, blow up and spit out information if
     # we don't get any query strings
-    elif not query:
+    else:
         buf = "Here are the possible query strings for \"/%s/%s\"<BR><BR>required_args:<BR>" % (pname, callpath)
         try:
-            for req in cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'].keys():
-                if cfg.debug:
-                    print "required arg: %s" % req
-                buf += "%s (%s): %s<BR>" % (req, cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['required_args']['args'][req]['desc'])
+            for req in pnameMetadata.metadata['methods'][callpath]['required_args']['args'].keys():
+                cm.debug("required arg: %s" % req)
+                buf += "%s (%s): %s<BR>" % (req, pnameCallpath['required_args']['args'][req]['vartype'], pnameCallpath['required_args']['args'][req]['desc'])
         except:
             buf += "No required_args found<BR>"
         try:
-            buf += "<BR><BR>optional_args, please supply at least %s but not more than %s of the following:<BR>" % (cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['min'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['max'])
-            for opt in cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'].keys():
-                if cfg.debug:
-                    print "optional arg: %s" % opt
-                if cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'] == "None":
-                    buf += "<A HREF=\"/%s/%s?%s\">/%s/%s?%s</A> (%s): %s<BR>" % (pname, callpath, opt, pname, callpath, opt, cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
+            buf += "<BR><BR>optional_args, please supply at least %s but not more than %s of the following:<BR>" % (pnameCallpath['optional_args']['min'], pnameCallpath['optional_args']['max'])
+            for opt in pnameCallpath['optional_args']['args'].keys():
+                cm.debug("optional arg: %s" % opt)
+                if pnameCallpath['optional_args']['args'][opt]['vartype'] == "None":
+                    buf += "<A HREF=\"/%s/%s?%s\">/%s/%s?%s</A> (%s): %s<BR>" % (pname, callpath, opt, pname, callpath, opt, pnameCallpath['optional_args']['args'][opt]['vartype'], pnameCallpath['optional_args']['args'][opt]['desc'])
                 else:
-                    buf += "/%s/%s?%s (%s): %s<BR>" % (pname, callpath, opt, cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['vartype'], cfg.module_metadata[pname].metadata['methods'][callpath]['optional_args']['args'][opt]['desc'])
+                    buf += "/%s/%s?%s (%s): %s<BR>" % (pname, callpath, opt, pnameCallpath['optional_args']['args'][opt]['vartype'], pnameCallpath['optional_args']['args'][opt]['desc'])
         except:
             buf += "<BR><BR>optional_args:<BR>No optional_args found<BR>"
         try:
-            buf += "<BR><BR>return:<BR>%s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath]['return'])
+            buf += "<BR><BR>return:<BR>%s" % myjson.JSONEncoder(indent=4).encode(pnameCallpath['return'])
         except:
             buf += "<BR><BR>return:<BR>ERROR: improperly formed return metadata"
-        if cfg.debug:
-            print buf
+        cm.debug(buf)
         return buf
-    # we got an actual callpath! do stuff.
-    else:
-        response.content_type='application/json'
-        if cfg.debug:
-            #print "method called: %s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath])
-            if bottle.request.method == cfg.module_metadata[pname].metadata['methods'][callpath]['rest_type']:
-                buf = getattr(cfg.module_metadata[pname], callpath)
-                returnme = buf(query)
-                print myjson.JSONEncoder(indent=4).encode(returnme)
-                return myjson.JSONEncoder().encode(returnme)
-            else:
-                raise ShipDaemonError("request method \"%s\" does not match allowed type \"%s\" for call \"/%s/%s\"" % (bottle.request.method, cfg.module_metadata[pname].metadata['methods'][callpath]['rest_type'], pname, callpath))
-        else:
-            if bottle.request.method == cfg.module_metadata[pname].metadata['methods'][callpath]['rest_type']:
-                buf = getattr(cfg.module_metadata[pname], callpath)
-                returnme = buf(query)
-                return myjson.JSONEncoder().encode(returnme)
-            else:
-                raise ShipDaemonError("request method \"%s\" does not match allowed type \"%s\" for call \"/%s/%s\"" % (bottle.request.method, cfg.module_metadata[pname].metadata['methods'][callpath]['rest_type'], pname, callpath))
 
+@httpship.route('/favicon.ico')
+def get_favicon():
+    return static_file('favicon.ico', root='static/')
+
+# run our module loader once at startup
+load_modules()
 # the daemon
 bottle.run(httpship, host='0.0.0.0', port=8081, reloader=False)
