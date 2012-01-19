c31af45fcd5f02a2c9b6b26d878dd48d08473fa4
Riccardo Setti <rsetti@gilt.com>
2012-01-19 14:36:58 -0500

    nojira: added setup.py again;

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import setuptools
+from setuptools import setup,  find_packages
+
+install_requires = [
+    'cmdln',
+    'SQLAlchemy == 0.5.5'
+    ]
+
+extras_require = {
+    'ldap' : 'python-ldap',
+    'postgres' : 'psycopg2'
+    }
+
+setup(name='Mothership',
+      author='Gilt SA team',
+      author_email='sa@gilt.com',
+      description='Mothership - asset managment',
+      packages=find_packages(),
+      scripts=['ship',
+               'ship_readonly'
+               ],
+      url='http://mothership.sf.net',
+      version='0.0.1',
+     )
