3d3a4372ba07c0e5bfa0fe89e74d4c45160c618a
david kovach <dkovach@gilt.com>
2012-01-20 16:58:12 +0000

    returning json and status=1 when we hit an exception

--- ship_daemon.py
+++ ship_daemon.py
@@ -29,6 +29,7 @@ httpship = bottle.Bottle()
 cfg = configure.Configure('mothership.yaml')
 cm = MothershipCommon('ship_daemon.log', to_file=False)
 
+
 # generic mothership exception type
 class ShipDaemonError(Exception):
     pass
@@ -47,6 +48,7 @@ def load_modules():
     """
     scans our main path for modules, loads valid modules
     """
+    response.content_type='application/json'
     cm.debug("loadmodules() called directly")
     # clear module metadata
     old_metadata = cfg.module_metadata
@@ -73,17 +75,25 @@ def load_modules():
                 cm.debug(foo)
                 bar = getattr(foo, i)
                 if cfg.debug:
-                    print bar
+                    cm.debug(bar)
                 inst = bar(cfg)
                 if cfg.debug:
-                    print inst
+                    cm.debug(inst)
                 cfg.module_metadata[i] = inst
             except Exception, e:
                 if cfg.debug:
-                    print "import error: %s" % e
-    except ImportError, e:
-        print "problem importing "+i
-        print "error: "+e
+                    cm.debug("import error: %s" % e)
+        jbuf = __generate_json_header()
+        jbuf['request'] = "/loadmodules"
+        jbuf['data'] = "reloaded modules:"
+        for k in cfg.module_metadata.keys():
+            jbuf['data'] += " %s" % cfg.module_metadata[k].namespace
+        return myjson.JSONEncoder().encode(jbuf)
+    except Exception, e:
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in load_modules(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
+
 
 
 @httpship.route('/')
@@ -96,18 +106,23 @@ def index():
     response.content_type='application/json'
     jbuf = __generate_json_header()
     jbuf['request'] = '/'
-    jbuf['data'] = "loaded modules: "
-    for k in cfg.module_metadata.keys():
-        cm.debug('route: /')
-        cm.debug('metadata key: '+k)
-        try:
-            jbuf['data'] += " %s" % cfg.module_metadata[k].namespace
-        except:
-            continue
-    jbuf['data'] += ". for more info on a module, call /<modulename>"
-    jbuf['data'] += ". to reload modules call: /loadmodules"
-    jbuf['data'] += ". to get JSON list of loaded modules call: /modules"
-    return myjson.JSONEncoder().encode(jbuf)
+    try:
+        jbuf['data'] = "loaded modules: "
+        for k in cfg.module_metadata.keys():
+            cm.debug('route: /')
+            cm.debug('metadata key: '+k)
+            try:
+                jbuf['data'] += " %s" % cfg.module_metadata[k].namespace
+            except:
+                continue
+        jbuf['data'] += ". for more info on a module, call /<modulename>"
+        jbuf['data'] += ". to reload modules call: /loadmodules"
+        jbuf['data'] += ". to get JSON list of loaded modules call: /modules"
+        return myjson.JSONEncoder().encode(jbuf)
+    except Exception, e:
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in index(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
 
 
 @httpship.route('/modules')
@@ -118,17 +133,22 @@ def loaded_modules():
     response.content_type='application/json'
     jbuf = __generate_json_header()
     jbuf['request'] = '/modules'
-    jbuf['data'] = []
-    for k in cfg.module_metadata.keys():
-        cm.debug('route: /')
-        cm.debug('metadata key: '+k)
-        try:
-            jbuf['data'].append(cfg.module_metadata[k].namespace)
-            cm.debug(jbuf)
-        except:
-            continue
-    response.content_type = 'application/json'
-    return myjson.JSONEncoder().encode(jbuf)
+    try:
+        jbuf['data'] = []
+        for k in cfg.module_metadata.keys():
+            cm.debug('route: /')
+            cm.debug('metadata key: '+k)
+            try:
+                jbuf['data'].append(cfg.module_metadata[k].namespace)
+                cm.debug(jbuf)
+            except:
+                continue
+        return myjson.JSONEncoder().encode(jbuf)
+    except Exception, e:
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in loaded_modules(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
+
 
 @httpship.route("/:pname")
 def namespace_path(pname):
@@ -138,15 +158,20 @@ def namespace_path(pname):
     response.content_type='application/json'
     jbuf = __generate_json_header()
     jbuf['request'] = "/%s" % pname
-    jbuf['data'] = {}
-    jbuf['data']['callable_functions'] = []
-    cm.debug("jbuf: %s" % jbuf)
-    cm.debug("pname: %s" %pname)
-    cm.debug("cfg.module_metadata[pname]: %s " %cfg.module_metadata[pname].metadata)
-    for meth in cfg.module_metadata[pname].metadata['methods']:
-        cm.debug(meth)
-        jbuf['data']['callable_functions'].append("/%s" % meth)
-    return myjson.JSONEncoder().encode(jbuf)
+    try:
+        jbuf['data'] = {}
+        jbuf['data']['callable_functions'] = []
+        cm.debug("jbuf: %s" % jbuf)
+        cm.debug("pname: %s" %pname)
+        cm.debug("cfg.module_metadata[pname]: %s " %cfg.module_metadata[pname].metadata)
+        for meth in cfg.module_metadata[pname].metadata['methods']:
+            cm.debug(meth)
+            jbuf['data']['callable_functions'].append("/%s" % meth)
+        return myjson.JSONEncoder().encode(jbuf)
+    except Exception, e:
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in namespace_path(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
 
 @httpship.route("/:pname/:callpath", method=('GET', 'POST', 'PUT', 'DELETE'))
 def callable_path(pname, callpath):
@@ -157,52 +182,66 @@ def callable_path(pname, callpath):
     response.content_type='application/json'
     jbuf = __generate_json_header()
     jbuf['request'] = "/%s/%s" % (pname, callpath)
-    query = bottle.request.GET
-    pnameMetadata = cfg.module_metadata[pname]
-
-    cm.debug("query keys: %s" % query.keys())
-    # everyone has a 'metadata' construct
-    # hard wire it into callpath options
-    if callpath == 'metadata':
-        cm.debug(myjson.JSONEncoder(indent=4).encode(pnameMetadata.metadata))
-        jbuf['data'] = pnameMetadata.metadata
-        return myjson.JSONEncoder().encode(jbuf)
-    else:
-        pnameCallpath = pnameMetadata.metadata['methods'][callpath]
-
-    # we got an actual callpath! do stuff.
-    if query:
-        cm.debug("method called: %s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath]))
-        if bottle.request.method == pnameCallpath['rest_type']:
-            buf = getattr(pnameMetadata, callpath)
-            jbuf['data'] = buf(query)
-            cm.debug(myjson.JSONEncoder(indent=4).encode(jbuf))
+    try:
+        query = bottle.request.GET
+        pnameMetadata = cfg.module_metadata[pname]
+        cm.debug("query keys: %s" % query.keys())
+        # everyone has a 'metadata' construct
+        # hard wire it into callpath options
+        if callpath == 'metadata':
+            cm.debug(myjson.JSONEncoder(indent=4).encode(pnameMetadata.metadata))
+            jbuf['data'] = pnameMetadata.metadata
             return myjson.JSONEncoder().encode(jbuf)
         else:
-            raise ShipDaemonError("request method \"%s\" does not match allowed type \"%s\" for call \"/%s/%s\"" % (bottle.request.method, pnameCallpath['rest_type'], pname, callpath))
-    # everyone wants query strings, blow up and spit out information if
-    # we don't get any query strings
-    else:
-        jbuf['data'] = {}
-        jbuf['data']['available_query_strings'] = {}
-        try:
-            for req in pnameMetadata.metadata['methods'][callpath]['required_args']['args'].keys():
-                cm.debug("required arg: %s" % req)
-            jbuf['data']['available_query_strings']['required_args'] = pnameCallpath['required_args']
-        except:
-            pass
-        try:
-            jbuf['data']['available_query_strings']['optional_args'] = pnameCallpath['optional_args']
-            for opt in pnameCallpath['optional_args']['args'].keys():
-                cm.debug("optional arg: %s" % opt)
-        except:
-            pass
-        return myjson.JSONEncoder(indent=4).encode(jbuf)
-        cm.debug(jbuf)
+            pnameCallpath = pnameMetadata.metadata['methods'][callpath]
+        # we got an actual callpath! do stuff.
+        if query:
+            cm.debug("method called: %s" % myjson.JSONEncoder(indent=4).encode(cfg.module_metadata[pname].metadata['methods'][callpath]))
+            if bottle.request.method == pnameCallpath['rest_type']:
+                buf = getattr(pnameMetadata, callpath)
+                jbuf['data'] = buf(query)
+                cm.debug(myjson.JSONEncoder(indent=4).encode(jbuf))
+                return myjson.JSONEncoder().encode(jbuf)
+            else:
+                raise ShipDaemonError("request method \"%s\" does not match allowed type \"%s\" for call \"/%s/%s\"" % (bottle.request.method, pnameCallpath['rest_type'], pname, callpath))
+        # everyone wants query strings, blow up and spit out information if
+        # we don't get any query strings
+        else:
+            jbuf['data'] = {}
+            jbuf['data']['available_query_strings'] = {}
+            try:
+                for req in pnameMetadata.metadata['methods'][callpath]['required_args']['args'].keys():
+                    cm.debug("required arg: %s" % req)
+                jbuf['data']['available_query_strings']['required_args'] = pnameCallpath['required_args']
+            except:
+                pass
+            try:
+                jbuf['data']['available_query_strings']['optional_args'] = pnameCallpath['optional_args']
+                for opt in pnameCallpath['optional_args']['args'].keys():
+                    cm.debug("optional arg: %s" % opt)
+            except:
+                pass
+            return myjson.JSONEncoder(indent=4).encode(jbuf)
+            cm.debug(jbuf)
+    except Exception, e:
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in callable_path(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
 
 @httpship.route('/favicon.ico')
 def get_favicon():
-    return static_file('favicon.ico', root='static/')
+    """
+    returns a favicon
+    """
+    try:
+        return static_file('favicon.ico', root='static/')
+    except Exception, e:
+        jbuf = __generate_json_header()
+        jbuf['request'] = "/favicon.ico"
+        jbuf['status'] = 1
+        jbuf['data'] = "Exception in get_favicon(). Error: %s" % e
+        return myjson.JSONEncoder().encode(jbuf)
+
 
 # run our module loader once at startup
 load_modules()
