f0c33103be1a8522024bdeab19446c3b5e9c5831
Riccardo Setti <rsetti@gilt.com>
2012-01-20 17:21:54 -0500

    Revert "nojira: split common..broke everything"
    This reverts commit 185bc27c0c852e78c23fa92a7b5f14f03d9e9355.

--- mothership/common/__init__.py
+++ mothership/common/__init__.py
@@ -4,25 +4,20 @@ import os
 MS_LOGDIR='/var/log/mothership/'
 
 class MothershipCommon(object):
-    def __init__(self):
-        pass
-    
+    def __init__(self, logfile='motherhship.log', debug_level='DEBUG', to_file=True):
+        if not os.path.exists(MS_LOGDIR):
+            os.mkdir(MS_LOGDIR)
+        if to_file:
+            logging.basicConfig(filename=MS_LOGDIR+logfile,level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger()
+        logger.setLevel(debug_level)
+
     def check_min_num_args(self, len, min):
         return True
     def check_max_num_args(self, len, max):
         return True
-
-    
-class MothershipLogging(object):
-    def __init__(self, cfg):
-        if not os.path.exists(cfg.logdir)
-            os.mkdir(cfg.logdir)
-        if cfg.to_file:
-            logging.basicConfig(filename=cfg.logdir+cfg.logfile,level=logging.DEBUG)
-        else:
-            logging.basicConfig(level=logging.DEBUG)
-        logger = logging.getLogger()
-        logger.setLevel(cfg.debug_level)
 
     def debug(self,message):
         logging.debug(message)
@@ -32,4 +27,3 @@ class MothershipLogging(object):
 
     def info(self, message):
         logging.info(message)
-    
