920e35b8f41e3416de3fbe70ab544327ee4d2c69
david kovach <downneck@gmail.com>
2012-02-01 12:55:20 -0500

    starting in on kv/collect tests

--- test/test_API_kv_bb.py
+++ test/test_API_kv_bb.py
@@ -274,3 +274,387 @@ class TestAPI_kv(unittest.TestCase):
         self.assertEqual(result, None)
         print "****** test_kv_select_value_not_found: PASSED"
 
+    
+    ######################################
+    # testing collect()                  #
+    ######################################
+
+    # test all=true, good results
+    def test_kv_collect_all_good(self):
+        query = {'all': True}
+        result = self.kv.collect(query)
+        
+        ret = [
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "359924B5-B1E4-468F-BD94-784FCD366BFA",
+                 "table_name": "kv",
+                 "key": "mc_secret",
+                 "id": 1
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "middleware",
+                 "table_name": "kv",
+                 "key": "mc_user",
+                 "id": 2
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "7AFAE79B-BFF1-4673-90DA-47A234CEC55B",
+                 "table_name": "kv",
+                 "key": "mc_userpass",
+                 "id": 3
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "satest",
+                 "table_name": "kv",
+                 "key": "environment",
+                 "id": 5
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "cobbler1",
+                 "table_name": "kv",
+                 "key": "mc_host",
+                 "id": 6
+               },
+               {
+                 "realm": "test",
+                 "hostname": "cobbler1",
+                 "site_id": "jfk",
+                 "value": "meddleware",
+                 "table_name": "kv",
+                 "key": "mc_admin",
+                 "id": 8
+               },
+               {
+                 "realm": "test",
+                 "hostname": "cobbler1",
+                 "site_id": "jfk",
+                 "value": "wheel",
+                 "table_name": "kv",
+                 "key": "groups",
+                 "id": 10
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "baseline",
+                 "table_name": "kv",
+                 "key": "class",
+                 "id": 11
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "zbx1",
+                 "table_name": "kv",
+                 "key": "zabbix_server",
+                 "id": 12
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "cobbler1",
+                 "site_id": "jfk",
+                 "value": "990DCAC9-65CC-4518-AA70-81CD153D1858",
+                 "table_name": "kv",
+                 "key": "mc_adminpass",
+                 "id": 14
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "cobbler1",
+                 "site_id": "jfk",
+                 "value": "meddleware",
+                 "table_name": "kv",
+                 "key": "mc_admin",
+                 "id": 15
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "/usr/libexec/openssh/ssh-ldap-helper.sh",
+                 "table_name": "kv",
+                 "key": "ssh_authorized_keys_command",
+                 "id": 19
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "nobody",
+                 "table_name": "kv",
+                 "key": "ssh_authorized_keys_command_run_as",
+                 "id": 20
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ldap1",
+                 "site_id": "jfk",
+                 "value": "topsecret",
+                 "table_name": "kv",
+                 "key": "ldap_master_slapd_rootpw",
+                 "id": 21
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ldap2",
+                 "site_id": "jfk",
+                 "value": "topsecret1",
+                 "table_name": "kv",
+                 "key": "ldap_slave_slapd_rootpw",
+                 "id": 22
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ci1",
+                 "site_id": "jfk",
+                 "value": "ree::bundler_gem",
+                 "table_name": "kv",
+                 "key": "class",
+                 "id": 25
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "xenserver1.satest.jfk",
+                 "table_name": "kv",
+                 "key": "ntp_servers",
+                 "id": 26
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "xenserver2.satest.jfk",
+                 "table_name": "kv",
+                 "key": "ntp_servers",
+                 "id": 27
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ldap1",
+                 "site_id": "jfk",
+                 "value": "root",
+                 "table_name": "kv",
+                 "key": "ldap_admin_cn",
+                 "id": 33
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ldap1",
+                 "site_id": "jfk",
+                 "value": "topsecret",
+                 "table_name": "kv",
+                 "key": "ldap_admin_pass",
+                 "id": 34
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "172.16.20.81,10.190.44.55,10.190.44.93",
+                 "table_name": "kv",
+                 "key": "ns",
+                 "id": 101
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "10 relay1,20 relay2",
+                 "table_name": "kv",
+                 "key": "mx",
+                 "id": 103
+               },
+               {
+                 "realm": None,
+                 "hostname": None,
+                 "site_id": None,
+                 "value": "sashimi911",
+                 "table_name": "kv",
+                 "key": "xen_api_pass",
+                 "id": 50
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "stage2",
+                 "site_id": "jfk",
+                 "value": "splunklightforwarder",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 38
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "cobbler1",
+                 "site_id": "jfk",
+                 "value": "splunklightforwarder",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 39
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ns1",
+                 "site_id": "jfk",
+                 "value": "splunklightforwarder",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 40
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "ns2",
+                 "site_id": "jfk",
+                 "value": "splunklightforwarder",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 41
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "decorati1",
+                 "site_id": "jfk",
+                 "value": "apache",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 49
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "jira1",
+                 "site_id": "jfk",
+                 "value": "mysql",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 51
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "jira1",
+                 "site_id": "jfk",
+                 "value": "java",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 52
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "mtest1",
+                 "site_id": "jfk",
+                 "value": "postgres",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 53
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "mtest3",
+                 "site_id": "jfk",
+                 "value": "postgres",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 54
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "mtest3",
+                 "site_id": "jfk",
+                 "value": "jira",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 55
+               },
+               {
+                 "realm": "satest",
+                 "hostname": "zbx1",
+                 "site_id": "jfk",
+                 "value": "test",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 57
+               },
+               {
+                 "realm": "satest",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "ldap1.satest.jfk.gilt.local",
+                 "table_name": "kv",
+                 "key": "ldap_master_server",
+                 "id": 71
+               },
+               {
+                 "realm": "satest",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "ldapsync",
+                 "table_name": "kv",
+                 "key": "ldap_sync_user",
+                 "id": 72
+               },
+               {
+                 "realm": "satest",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "aJLxA33Ng",
+                 "table_name": "kv",
+                 "key": "ldap_sync_pass",
+                 "id": 73
+               },
+               {
+                 "realm": "prod",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "ldap1.satest.jfk.gilt.local",
+                 "table_name": "kv",
+                 "key": "ldap_master_server",
+                 "id": 74
+               },
+               {
+                 "realm": "prod",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "ldapsync",
+                 "table_name": "kv",
+                 "key": "ldap_sync_user",
+                 "id": 75
+               },
+               {
+                 "realm": "prod",
+                 "hostname": None,
+                 "site_id": "jfk",
+                 "value": "aJLxA33Ng",
+                 "table_name": "kv",
+                 "key": "ldap_sync_pass",
+                 "id": 76
+               },
+               {
+                 "realm": "prod",
+                 "hostname": "cm1",
+                 "site_id": "iad",
+                 "value": "postgres",
+                 "table_name": "kv",
+                 "key": "tag",
+                 "id": 88
+               }
+        ]
+
+        self.assertEqual(result, ret)
+        print "****** test_kv_collect_all_good: PASSED"
