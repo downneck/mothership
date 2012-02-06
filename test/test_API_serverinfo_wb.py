e911e33cfbe5acb8245fbf09afda8bcaa5c6da33
david kovach <downneck@gmail.com>
2012-02-06 15:51:54 -0500

    updating API_serverinfo whitebox tests for standardised testing db

--- test/test_API_serverinfo_wb.py
+++ test/test_API_serverinfo_wb.py
@@ -8,33 +8,39 @@ from mothership.common import *
 
 from sqlalchemy import or_, desc, MetaData
 
+# globals
+# load up the config object
+cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
+cfg.load_config()
+# override logging parameters for testing
+cfg.log_to_file = False
+cfg.log_to_console = False
+# instantiate a logger so the module we're testing doesn't freak out
+cfg.log = MothershipLogger(cfg)
+# instantiate the main class
+si = API_serverinfo(cfg)
+
+
 class TestAPI_serverinfo(unittest.TestCase):
     def test_get_host_from_hwtag(self):
-        cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
-        cfg.load_config()
-        cfg.log = MothershipLogger(cfg) 
-        si = API_serverinfo(cfg)
-        result = si._get_host_from_hwtag('3Y29JQ1')
+        result = si._get_host_from_hwtag('4VK27L1')
 
         s = si.cfg.dbsess.query(Server).\
-            filter(Server.hw_tag=='3Y29JQ1').\
+            filter(Server.hw_tag=='4VK27L1').\
             filter(Server.virtual==False).first()
         if s.hostname:
             ret = si._get_serverinfo(s.hostname, s.realm, s.site_id)
 
-        self.assertDictEqual(result, ret)
+        self.assertEqual(result, ret)
+        print "****** test_serverinfo_get_host_from_hwtag: PASSED"
 
     def test_get_host_from_ip(self):
-        cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
-        cfg.load_config()
-        cfg.log = MothershipLogger(cfg) 
-        si = API_serverinfo(cfg)
-        result = si._get_host_from_ip('10.60.0.20')
+        result = si._get_host_from_ip('10.190.44.52')
 
         try:
             s, n = si.cfg.dbsess.query(Server, Network).\
                    filter(Server.id==Network.server_id).\
-                   filter(Network.ip=='10.60.0.20').first()
+                   filter(Network.ip=='10.190.44.52').first()
             if s.hostname:
                 ret = si._get_serverinfo(s.hostname, s.realm, s.site_id)
         except TypeError:
@@ -43,41 +49,36 @@ class TestAPI_serverinfo(unittest.TestCase):
         try:
             s, n = si.cfg.dbsess.query(Server, Network).\
                    filter(Server.id==Network.server_id).\
-                   filter(Network.public_ip=='10.60.0.20').first()
+                   filter(Network.public_ip=='10.190.44.52').first()
             if s.hostname:
                 ret = si._get_serverinfo(s.hostname, s.realm, s.site_id)
         except TypeError:
             pass
         
-        self.assertDictEqual(ret, result)
+        self.assertEqual(ret, result)
+        print "****** test_serverinfo_get_host_from_ip: PASSED"
         
     def test_got_host_from_mac(self):
-        cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
-        cfg.load_config()
-        cfg.log = MothershipLogger(cfg) 
-        si = API_serverinfo(cfg)
-        result = si._get_host_from_mac('14:fe:b5:db:0a:58')
+        result = si._get_host_from_mac('00:21:9b:98:49:24')
 
         h, s = cfg.dbsess.query(Network, Server).\
                filter(Network.hw_tag==Server.hw_tag).\
-               filter(Network.mac=='14:fe:b5:db:0a:58').\
+               filter(Network.mac=='00:21:9b:98:49:24').\
                filter(Server.virtual==False).first()
         if s.hostname:
             ret = si._get_serverinfo(s.hostname, s.realm, s.site_id)
 
-        self.assertDictEqual(result, ret)
+        self.assertEqual(result, ret)
+        print "****** test_serverinfo_get_host_from_mac: PASSED"
 
     def test_got_host_from_hostname(self):
-        cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
-        cfg.load_config()
-        cfg.log = MothershipLogger(cfg) 
-        si = API_serverinfo(cfg)
         result = si._get_host_from_hostname('cm1')
 
         s = mothership.validate.v_get_host_obj(cfg, 'cm1')
         ret = si._get_serverinfo(s.hostname, s.realm, s.site_id)
 
-        self.assertDictEqual(result, ret)
+        self.assertEqual(result, ret)
+        print "****** test_serverinfo_get_host_from_hostname: PASSED"
         
     def test_get_serverinfo(self):
         pass
