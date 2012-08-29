4fd13a380a3b4d85b333287d7d5d886e2c36ff79
david kovach <downneck@gmail.com>
2012-08-29 15:09:12 -0400

    adding initial tests for userdata

---
+++ test/test_API_userdata_bb.py
@@ -0,0 +1,74 @@
+import unittest
+
+from mothership.API_userdata import * 
+from mothership.configure import *
+from mothership.common import *
+
+
+# globals
+###
+# load up the config object
+cfg = MothershipConfigureDaemon('mothership_daemon.yaml')
+cfg.load_config()
+# override logging parameters for testing
+cfg.log_to_file = False 
+cfg.log_to_console = False
+# instantiate a logger so the module we're testing doesn't freak out
+cfg.log = MothershipLogger(cfg)
+# instantiate the main class
+ud = API_userdata(cfg)
+cfg.module_metadata['API_userdata'] = ud
+
+# UnitTesting for API_userdata module
+class TestAPI_userdata(unittest.TestCase):
+
+    ######################################
+    # testing udisplay()                 #
+    ######################################
+
+    # good input, good output
+    def test1(self):
+        query = {'unqun': 'bobsponge.satest.jfk'}
+        result = ud.udisplay(query)
+
+        # pre-define expected output
+        ret = {
+               'username': 'bobsponge',
+               'hdir': '/home/bobsponge',
+               'first_name': 'bob',
+               'last_name': 'two',
+               'realm': 'satest',
+               'uid': 639,
+               'site_id': 'jfk',
+               'id': 175,
+               'shell': '/bin/bash',
+               'ssh_public_key': None,
+               'active': True,
+               'type': 'employee',
+               'email': 'bobsponge@gilt.com' 
+        }
+
+        self.assertEqual(result, ret)
+        print "[API_userdata] test1: PASSED"
+
+    # bad username, error raised 
+    def test2(self):
+        query = {'unqdn': 'garbage.satest.jfk'}
+
+        self.assertRaises(UserdataError, ud.udisplay, query)
+        print "[API_userdata] test2: PASSED"
+
+    # bad realm, error raised 
+    def test3(self):
+        query = {'unqdn': 'bobsponge.garbage.jfk'}
+
+        self.assertRaises(UserdataError, ud.udisplay, query)
+        print "[API_userdata] test3: PASSED"
+
+    # bad site_id, error raised 
+    def test4(self):
+        query = {'unqdn': 'bobsponge.satest.garbage'}
+
+        self.assertRaises(UserdataError, ud.udisplay, query)
+        print "[API_userdata] test4: PASSED"
+
